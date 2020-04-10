5cb1b1ed257a45a5646d68c7fa7f8af73dcfe78c
shreyaasridhar <shreyanju@gmail.com>
2020-04-10 12:53:34 -0700

    Added MapView component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import Profile from "./components/Profile";
-
+import MapContainer from "./components/MapView";
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
+          <Route path="/map">
+            <MapContainer />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
